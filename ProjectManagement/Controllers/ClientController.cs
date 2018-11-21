67bd4289fa683123292fda62a5bdc88b8a6aa078
ideki11 <ideki0588@gmail.com>
2018-11-21 16:05:23 +0700

    push

---
+++ ProjectManagement/Controllers/ClientController.cs
@@ -0,0 +1,34 @@
+﻿using ProjectManagement.Entites;
+using ProjectManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class ClientController : Controller
+    {
+        ProjectManagementDbContext db = new ProjectManagementDbContext();
+        public ActionResult Index()
+        {
+            //test Commit
+            return View();
+        }
+
+        public JsonResult Read()
+        {
+            List<ClientViewModel> GetClientList = db.Clients.Select(x => new ClientViewModel
+            {
+                ClientId = x.ClientId,
+                ContactPerson = x.ContactPerson,
+                Email = x.Email,
+                Name = x.Name,
+                Phone = x.Phone
+            }).ToList();
+
+            return Json(GetClientList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
